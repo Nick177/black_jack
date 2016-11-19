5a9721f9e3fddfd43ff1496c8ee2b19630621382
Nicholas Rosas <nrosas@csumb.edu>
2016-11-19 09:49:55 -0800

    Init Deck class

--- app/src/main/java/com/example/nick/black_jack/Deck.java
+++ app/src/main/java/com/example/nick/black_jack/Deck.java
@@ -1,8 +1,29 @@
 package com.example.nick.black_jack;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Created by Kristine on 11/19/16.
  */
 
 public class Deck {
+    private Deque<Integer> cards;
+    private int size;
+    private int initId;
+
+
+    public Deck(int initId) {
+        this.size = size;
+        this.initId = initId;
+        int imgId = initId;
+
+        cards = new LinkedList<Integer>();
+
+        for(int ix = 1; ix <= 13; ix++) {
+            for(int jx = 0; jx < 4; jx++) {
+               // Card card = new Card(ix, imgId);
+            }
+        }
+    }
 }
